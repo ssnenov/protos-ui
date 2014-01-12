89220f74fa25c7621dfa46934bdd267d2ef4723a
ssnenov <lord_simeon@abv.bg>
2014-01-12 21:54:49 +0200

    Changed the master branch

--- src/scrollTo.js
+++
@@ -1,7 +0,0 @@
-protos.scrollTo = function(options) {
-	var author = options.author,
-		speed = options.speed || 1000;
-	$(document.body).animate({ scrollTop: protos.getElementOffset(author[0]).top }, speed);
-	
-	return author;
-};
